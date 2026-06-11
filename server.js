7e63a93deffeab8c0d2dbeb5eb439e3c442be432
Ankit <ankitsahuas958080@gmail.com>
2026-06-11 09:07:07 +0530

    fix

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
 
-const http = require("http").createServer(app);
+const server = http.createServer(app);
 
-const io = require("socket.io")(http);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
 let onlineUsers = 0;
 
-io.on("connection", (socket) => {
+io.on("connection", (socket)=>{
 
-    // user connected
     onlineUsers++;
 
     io.emit("online-users", onlineUsers);
 
-    // send message
-    socket.on("send-message", (data) => {
+    socket.on("send-message", (data)=>{
 
         io.emit("receive-message", data);
 
     });
 
-    // edit message
-    socket.on("edit-message", (data) => {
+    socket.on("edit-message", (data)=>{
 
         io.emit("message-edited", data);
 
     });
 
-    // disconnect
-    socket.on("disconnect", () => {
+    socket.on("disconnect", ()=>{
 
         onlineUsers--;
 
@@ -41,7 +40,7 @@ io.on("connection", (socket) => {
 
 });
 
-http.listen(3000, "0.0.0.0", () => {
+server.listen(3000, "0.0.0.0", ()=>{
 
     console.log("Running");
 
