fd926046a44c10821eb61d93367e632cf0ca1f45
Ankit <ankitsahuas958080@gmail.com>
2026-06-11 09:28:12 +0530

    fixed all

--- public/script.js
+++ public/script.js
@@ -1,95 +1,144 @@
 const socket = io();
 
-const messages = document.getElementById("messages");
-const input = document.getElementById("message");
 const nameInput = document.getElementById("name");
+const messageInput = document.getElementById("message");
+const messages = document.getElementById("messages");
 const onlineUsers = document.getElementById("onlineUsers");
 
-function sendMessage() {
+function sendMessage(){
 
-    const message = input.value.trim();
     const name = nameInput.value.trim();
+    const message = messageInput.value.trim();
 
-    if(message === "" || name === "") return;
+    if(name === "" || message === ""){
+        return;
+    }
 
     const data = {
+
         id: Date.now(),
+
+        sender: socket.id,
+
         name: name,
+
         message: message,
-        time: new Date().toLocaleTimeString(),
-        sender: socket.id
+
+        time: new Date().toLocaleTimeString()
+
     };
 
     socket.emit("send-message", data);
 
-    input.value = "";
+    messageInput.value = "";
+
 }
 
-socket.on("receive-message", (data) => {
+socket.on("receive-message", (data)=>{
 
     const div = document.createElement("div");
 
     if(data.sender === socket.id){
+
         div.className = "my-message";
-    } else {
+
+    }else{
+
         div.className = "other-message";
-    }
 
-    div.id = data.id;
-
-    let editButton = "";
-
-    if(data.sender === socket.id){
-
-        editButton = `
-            <button onclick="editMessage('${data.id}')">
-                Edit
-            </button>
-        `;
     }
 
     div.innerHTML = `
-        <h3>${data.name}</h3>
-        <p class="text">${data.message}</p>
-        <small>${data.time}</small>
-        ${editButton}
+
+        <div class="name">${data.name}</div>
+
+        <div>${data.message}</div>
+
+        <div class="time">${data.time}</div>
+
     `;
+
+    if(data.sender === socket.id){
+
+        const editBtn = document.createElement("button");
+
+        editBtn.innerText = "Edit";
+
+        editBtn.className = "edit-btn";
+
+        editBtn.onclick = ()=>{
+
+            const newMessage = prompt("Edit message", data.message);
+
+            if(newMessage){
+
+                data.message = newMessage;
+
+                socket.emit("edit-message", data);
+
+            }
+
+        };
+
+        div.appendChild(editBtn);
+
+    }
+
+    div.setAttribute("data-id", data.id);
 
     messages.appendChild(div);
 
     messages.scrollTop = messages.scrollHeight;
+
 });
-
-function editMessage(id){
-
-    const box = document.getElementById(id);
-
-    const text = box.querySelector(".text");
-
-    const newText = prompt("Edit message", text.innerText);
-
-    if(newText){
-
-        text.innerText = newText;
-
-        socket.emit("edit-message", {
-            id: id,
-            newMessage: newText
-        });
-
-    }
-
-}
 
 socket.on("message-edited", (data)=>{
 
-    const box = document.getElementById(data.id);
+    const allMessages = document.querySelectorAll("[data-id]");
 
-    if(box){
+    allMessages.forEach((msg)=>{
 
-        box.querySelector(".text").innerText = data.newMessage;
+        if(msg.getAttribute("data-id") == data.id){
 
-    }
+            msg.innerHTML = `
+
+                <div class="name">${data.name}</div>
+
+                <div>${data.message}</div>
+
+                <div class="time">${data.time} (edited)</div>
+
+            `;
+
+            if(data.sender === socket.id){
+
+                const editBtn = document.createElement("button");
+
+                editBtn.innerText = "Edit";
+
+                editBtn.className = "edit-btn";
+
+                editBtn.onclick = ()=>{
+
+                    const newMessage = prompt("Edit message", data.message);
+
+                    if(newMessage){
+
+                        data.message = newMessage;
+
+                        socket.emit("edit-message", data);
+
+                    }
+
+                };
+
+                msg.appendChild(editBtn);
+
+            }
+
+        }
+
+    });
 
 });
 
@@ -99,15 +148,12 @@ socket.on("online-users", (count)=>{
 
 });
 
-input.addEventListener("keypress", (e)=>{
+messageInput.addEventListener("keypress", (e)=>{
 
     if(e.key === "Enter"){
+
         sendMessage();
+
     }
 
 });
-if(data.sender === socket.id){
-    div.className = "my-message";
-} else {
-    div.className = "other-message";
-}
--- server.js
+++ server.js
@@ -1,35 +1,34 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
 
-const io = new Server(server);
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
 let users = 0;
 
-io.on("connection", (socket) => {
+io.on("connection", (socket)=>{
 
     users++;
 
     io.emit("online-users", users);
 
-    socket.on("send-message", (data) => {
+    socket.on("send-message", (data)=>{
 
         io.emit("receive-message", data);
 
     });
 
-    socket.on("edit-message", (data) => {
+    socket.on("edit-message", (data)=>{
 
         io.emit("message-edited", data);
 
     });
 
-    socket.on("disconnect", () => {
+    socket.on("disconnect", ()=>{
 
         users--;
 
@@ -39,8 +38,10 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, "0.0.0.0", () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Running");
+http.listen(PORT, "0.0.0.0", ()=>{
+
+    console.log("Server Running");
 
 });
